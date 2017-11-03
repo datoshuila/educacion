e45dbb519fe82674ea978eeddeeff3b5c93eeec2
cecabrera <cecabrera55@gmail.com>
2017-11-03 17:49:50 -0500

    Mejora en la consulta de datos del archivo aprobados.sql

--- data/aprobados.sql
+++ data/aprobados.sql
@@ -1,13 +1,14 @@
-SELECT
-    m.mun_nombre as Municipio
-    , c.coal_anio as Ano
+﻿SELECT
+    m.mun_nombre as "Municipio"
+    , c.coal_anio as "Ano"
     , ti.tpin_nombre as "Tipo Institucion"
     , c.coal_aprobados as "N_Aprobados"
     , c.coal_reprobados as "N_Reprobados"
     , c.coal_desertores as "N_Desertores"
+    , c.coal_traslados as "N_Traslados"
     , c.coal_porce_aprobados as "P_Aprobados"
     , c.coal_porce_reprobados as "P_Reprobados"
-    , c."coal_porce_estudiantes desertores" as "P_Desertores"
+    , c.coal_porce_desertores as "P_Desertores"
     , c.coal_porce_traslados as "P_Traslados"
 FROM "Educacion"."comportamiento_alumnos" AS c
 LEFT JOIN "public"."municipio" AS m ON c."coal_codMunicipio" = m.mun_codigo

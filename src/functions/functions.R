630fc858b1623a1981664ac6f3e44771f1244ed8
cecabrera <cecabrera55@gmail.com>
2017-11-24 08:29:34 -0500

    Función getSQL

--- src/functions/functions.R
+++ src/functions/functions.R
@@ -1,3 +1,24 @@
+# ---- Load functions ----
+
+# ---- getSQL: get a filepath to an SQL file and converts it into string
+getSQL <- function(filepath){
+    con = file(filepath, "r")
+    sql.string <- ""
+    
+    while ( TRUE ){
+        line <- readLines(con, n = 1)
+        if ( length(line) == 0 ) { break }
+        line <- gsub("\\t", " ", line)
+        if(grepl("--",line) == TRUE){
+            line <- paste(sub("--","/*",line),"*/")
+        }
+        sql.string <- paste(sql.string, line)
+    }
+    
+    close(con)
+    return(sql.string)
+}
+
 renderBox <- function(title, icon, color, data){
     renderValueBox({ valueBox(
         value = tags$p(title, style = "font-size: 60%;")
